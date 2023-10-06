7928e9a80b78662094ce5f9a8c089a325ca3b8e1
Joacofb <joaquinbrischetto@gmail.com>
2023-10-06 15:53:40 +0100

    Exercices up to 3.22

--- part3/index.js
+++ part3/index.js
@@ -1,117 +1,113 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+
 const app = express()
 
 require('dotenv').config()
-const mongoose = require('mongoose')
 const Person = require('./models/person')
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-morgan.token('data', (req, res) => {
-    return [
-        req.method,
-        req.url,
-        JSON.stringify(req.body)
-    ].join(' ')
-})
+morgan.token('data', (req, res) => [
+  req.method,
+  req.url,
+  JSON.stringify(req.body),
+].join(' '))
 
 app.use(morgan(':data'))
 
-let persons = []
-
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
-  
+  res.send('<h1>Hello World!</h1>')
+})
+
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(people => {
-        res.json(people)
-    })
+  Person.find({}).then((people) => {
+    res.json(people)
+  })
 })
 
 app.get('/info', (req, res) => {
-    const date = new Date()
-    Person.find({}).then(people => {
-        res.send(`
+  const date = new Date()
+  Person.find({}).then((people) => {
+    res.send(`
             <h2>Your phonebook has info for ${people.length} people.</h2>
             <h4>${date}</h4>
         `)
-    })
+  })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person
-        .findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person
+    .findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
-    console.log(body)
+  const { body } = req
+  console.log(body)
 
-    const person = new Person ({
-        name: body.name,
-        number: body.number,
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson.toJSON())
     })
-
-    person
-        .save()
-        .then(savedPerson => {
-            res.json(savedPerson.toJSON())
-        })
-        .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+  const { body } = req
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new:true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
